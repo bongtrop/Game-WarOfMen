fcb082889467b68111024439704d2d444e13af95
bby15603 <goleto@hotmail.com>
2013-08-23 12:51:46 +0700

    First Commit

---
+++ position.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class position here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class position
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for objects of class position
+     */
+    public position(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
+}
---
+++ state.java
@@ -0,0 +1,50 @@
+
+/**
+ * Write a description of class state here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class state implements iState
+{
+    // instance variables - replace the example below with your own
+    private int nstate;
+    private int state[5][15][20];
+    public static final int WATER = 0;
+    public static final int EARTH = 1;
+    public static final int BREAKWOOD = 2;
+    public static final int WOOD = 3;
+    public static final int STONE = 4;
+    public static final int BRIDGE = 5;
+
+    /**
+     * Constructor for objects of class state
+     */
+    public state(int nstate)
+    {
+        // initialise instance variables
+        this.nstate = nstate;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int setState(int nstate)
+    {
+        // put your code here
+        this.nstate = nstate;
+    }
+    
+    public int getSymbol(position pos) {
+        return this.state[nstate][pos.getY()][pos.getX()];
+    }
+    
+    public void updateState(position pos) {
+        ...
+    }
+    
+    
+}
